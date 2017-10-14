82c1d46b8b4b604708361290e125d3ce016ae8e9
Chi Lam <tonje_@hotmail.co.uk>
2017-10-14 02:40:38 +0200

    first commit

---
+++ ERP.Bootstrap/Registries/ERPRegistry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+using ERP.Presenters.Bases;
+using ERP.Model;
+
+namespace ERP.Bootstrap.Registries
+{
+    public class ERPRegistry : Registry
+    {
+        public ERPRegistry()
+        {
+            Scan(scanner =>
+            {
+                scanner.RegisterConcreteTypesAgainstTheFirstInterface();     
+                scanner.AssemblyContainingType<PresenterBase>();             
+                scanner.AssemblyContainingType<WelcomeModel>();              
+                scanner.WithDefaultConventions();                            
+                scanner.SingleImplementationsOfInterface();                  
+            });
+
+            //here we define the classes to be resolved and configuure the container to search through the
+            //assembly which contains our PresenterBase class - which is the common project dir. All Classes that have 
+            // a corresponding Interface where the naming conventions are Classname - IClassname will be added to the reg.
+            // we will need to add a pointer class to include an assembly for any other classes that require an interface dependency
+            // to be resolved if the assembly isn't already included.
+            // like the assembly which contains the models ect...
+        }
+    }
+}
---
+++ ERP.Views/WelcomeView.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WelcomeView.aspx.cs" Inherits="ERP.Views.WelcomeView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head runat="server">
+    <title>
+    </title>  
+</head>
+
+<style type="text/css">
+
+body{
+    color: darkslategray;
+    background: cornsilk;
+}
+form1{
+    color:darkslategrey;
+}
+.WelcomeTitle{
+    position: absolute;
+    left: 32%;
+    top: 10%;
+    font-size: 100px;
+    font-weight: bolder;
+}
+.WelcomeMessage{
+    position: absolute;
+    left: 34%;
+    top: 39%;
+    font-size: 35px;
+}
+.GoToLoginButton{
+    position: absolute;
+    top: 67%;
+    left: 40%;
+    font-size: 44px;
+    font-variant:small-caps;
+    background: honeydew;
+    border-radius: 14px;
+    border-bottom-width: thick;
+}
+</style>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Label ID="txtWelcomeTitle" CssClass="WelcomeTitle" runat="server" Text=""></asp:Label>
+    <asp:Label ID="txtWelcomeMessage" CssClass="WelcomeMessage" runat="server" Text=""></asp:Label>
+    <asp:Button ID="btnGoToLoginScreen" CssClass="GoToLoginButton" runat="server" Text="Go To Login" /> 
+    </div>
+    </form>
+</body>
+</html>
